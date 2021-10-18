2c5bd4b791ef50a580f34bee212dbd74593c3cff
Yaroslav Ryabovol <92582460+Senolluks@users.noreply.github.com>
2021-10-18 19:30:42 +0300

    Add files via upload

---
+++ Lesson 1/task 1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{   cout << "Hello Yaroslav! "<< endl ;
+    cout << "Have a good day!" ;
+    return 0;
+}
---
+++ Lesson 1/task 4/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b, c, D, x1, x2;
+    cout << "„B„r„u„t„y„„„u 3 „‰„y„ƒ„|„p:" << endl;
+    cin >> a >> b >> c;
+
+    D = b^2 - 4 * a * c;
+
+    if(D > 0){
+       x1 = ((-b) + sqrt(D)) / (2 * a);
+      cout << "x1=" << x1 << endl;
+       x2 = ((-b) - sqrt(D)) / (2 * a);
+      cout << "x2= "<< x2 << endl;
+
+    }
+    if(D < 0){
+        cout << "„K„€„‚„~„u„z „~„u„„!";
+    }
+
+    if(D == 0){
+       x1= -(b / (2 * a));
+      cout << "x1- " << x1 << endl;
+
+    }
+
+
+    return 0;
+}
